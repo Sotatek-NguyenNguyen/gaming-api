bbe7c8f1f1661df9b23c440e8ca6561455be2f82
Duong <duong.dang2@sotatek.com>
2021-12-21 17:22:37 +0700

    chore: validate transactionId in ListUserTransactionHistoryQuery

--- src/modules/balance-change/dto/get-current-user-deposit-history.query.ts
+++ src/modules/balance-change/dto/get-current-user-deposit-history.query.ts
@@ -32,6 +32,7 @@ export class ListUserTransactionHistoryQuery extends PaginationQueryDto {
   @ApiPropertyOptional({
     example: '3MNh8ihk5XfweiJvBdQtLkecnbGvzA6p7vQESuqCvC5fVZ7mLoadytJwdVg9XpVfBTvsnQbUZ9eumvqST7vJ3RMT',
   })
+  @IsString({ optional: true })
   transactionId: string;
 
   @ApiPropertyOptional({ enum: TransactionType })
