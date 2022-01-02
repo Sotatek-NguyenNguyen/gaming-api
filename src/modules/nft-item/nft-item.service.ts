6417c54a3542849262d65d838503da76f2879a8a
Sotatek-DucPham <duc.pham@sotatek.com>
2022-01-03 00:45:49 +0700

    feat: batch request

--- src/modules/nft-item/nft-item.service.ts
+++ src/modules/nft-item/nft-item.service.ts
@@ -1,14 +1,20 @@
 import { Injectable } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { AnyKeys, FilterQuery, Model } from 'mongoose';
+import { Client } from 'jayson';
 import { IDataWithPagination } from 'src/common/interfaces';
 import { INftFilter } from './interfaces';
 import { NftItem, NftItemDocument } from './nft-item.schema';
 import { sleep } from 'src/common/utils';
+import { ApiConfigService } from '../shared/services';
 
 @Injectable()
 export class NftItemService {
-  constructor(@InjectModel(NftItem.name) readonly model: Model<NftItemDocument>) {}
+  private jaysonClient: Client;
+
+  constructor(@InjectModel(NftItem.name) readonly model: Model<NftItemDocument>, configService: ApiConfigService) {
+    this.jaysonClient = Client.https({ host: 'api.devnet.solana.com' });
+  }
 
   async list(filter: INftFilter): Promise<IDataWithPagination<NftItem>> {
     const query = this._genQueryFromRequestFilter(filter);
@@ -64,7 +70,22 @@ export class NftItemService {
           .skip((page - 1) * pageSize)
           .limit(pageSize)
           .lean({ virtuals: true });
-        console.log(nfts);
+        if (nfts.length === 0) {
+          page = 1;
+          break;
+        }
+
+        const nftIds = nfts.map((nft) => nft.address);
+        const batch = nftIds.map((nftId) =>
+          this.jaysonClient.request('getTokenLargestAccounts', [nftId, { commitment: 'finalized' }]),
+        );
+        await this.jaysonClient.request(batch, function (err, errors, successes) {
+          if (err) throw err;
+          console.log('errors', errors); // array of requests that errored
+          console.log('successes', successes); // array of requests that succeeded
+        });
+        // const info = await this.finalizeConnection.getTokenLargestAccounts(new PublicKey(nft.address));
+        // console.log(info);
 
         if (nfts.length < pageSize) {
           page = 1;
@@ -77,7 +98,7 @@ export class NftItemService {
 
     while (true) {
       await innerFunction();
-      await sleep(5 * 1000);
+      await sleep(60 * 1000);
     }
   }
 }
