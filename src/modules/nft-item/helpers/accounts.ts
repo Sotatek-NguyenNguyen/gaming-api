826816b66eea418155ffbcf92db24996a687c061
Duong <duong.dang2@sotatek.com>
2022-01-07 08:52:41 +0700

    chore: nft-item/helper/accounts

--- src/modules/nft-item/helpers/accounts.ts
+++ src/modules/nft-item/helpers/accounts.ts
@@ -1,4 +1,3 @@
-import anchor from '@project-serum/anchor';
 import { PublicKey } from '@solana/web3.js';
 import { SPL_ASSOCIATED_TOKEN_ACCOUNT_PROGRAM_ID, TOKEN_METADATA_PROGRAM_ID, TOKEN_PROGRAM_ID } from './constants';
 
@@ -11,18 +10,18 @@ export const getTokenWallet = async function (wallet: PublicKey, mint: PublicKey
   )[0];
 };
 
-export const getMetadata = async (mint: anchor.web3.PublicKey): Promise<anchor.web3.PublicKey> => {
+export const getMetadata = async (mint: PublicKey): Promise<PublicKey> => {
   return (
-    await anchor.web3.PublicKey.findProgramAddress(
+    await PublicKey.findProgramAddress(
       [Buffer.from('metadata'), TOKEN_METADATA_PROGRAM_ID.toBuffer(), mint.toBuffer()],
       TOKEN_METADATA_PROGRAM_ID,
     )
   )[0];
 };
 
-export const getMasterEdition = async (mint: anchor.web3.PublicKey): Promise<anchor.web3.PublicKey> => {
+export const getMasterEdition = async (mint: PublicKey): Promise<PublicKey> => {
   return (
-    await anchor.web3.PublicKey.findProgramAddress(
+    await PublicKey.findProgramAddress(
       [Buffer.from('metadata'), TOKEN_METADATA_PROGRAM_ID.toBuffer(), mint.toBuffer(), Buffer.from('edition')],
       TOKEN_METADATA_PROGRAM_ID,
     )
